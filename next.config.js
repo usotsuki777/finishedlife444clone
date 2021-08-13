9824b5029f3ad75eea419e528abfd5f24e68839b
usotsuki777 <fillip4streaming@gmail.com>
2021-08-14 05:31:08 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  assetPrefix:
+    process.env.NODE_ENV === 'production' ? '/finishedlife444clone' : '',
+};
